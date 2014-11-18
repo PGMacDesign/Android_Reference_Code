d00431e069760901a953702addffbe0470601916
PGMacDesign <pgmacdesign@gmail.com>
2014-11-18 10:53:28 -0800

    Daily update with adding new features

---
+++ Java Files/Show Options Menu Within a Fragment.java
@@ -0,0 +1,8 @@
+
+//Add this method in or the fragment will not allow for an options menu to be present
+public theFragment(){
+	setHasOptionsMenu(true);
+}
+
+
+//Still needs code for implementation 
