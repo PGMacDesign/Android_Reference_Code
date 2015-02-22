30ee4a97e3424c6fd7d9079cc9d0327108a3eb31
pgmacdesign <pgmacdesign@gmail.com>
2015-02-22 00:47:36 -0800

    Adding picasso example

---
+++ Java Files/Set Photos With Picasso.java
@@ -0,0 +1,4 @@
+//Don't forget to include the library (in build.gradle = )
+
+//Context, image url (IE https://avatars0.githubusercontent.com/u/6592650?v=3&s=460), the image view UI component
+Picasso.with(context).load(image_url).into(imageView);
