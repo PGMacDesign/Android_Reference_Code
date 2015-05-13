3c2739be54675613b1bebbcb8d674779feee88d7
Patrick MacDowell <PGMacDesign@users.noreply.github.com>
2015-05-13 15:31:50 -0700

    Update Intent - Not To Leave User In Different Application.java

--- Java Files/Intent - Not To Leave User In Different Application.java
+++ Java Files/Intent - Not To Leave User In Different Application.java
@@ -2,6 +2,8 @@
 //When I open a link, IE in pulse, and it opens up to chrome and then go to the home screen, if I click on pulse again, having this flag in it will PREVENT the app from opening up into chrome, it will open into pulse.
 
 Intent shareIntent = new Intent(Intent.ACTION_SEND);
-shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET); //THIS IS THE KEY LINE HERE
+shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT ); //THIS IS THE KEY LINE HERE
 shareIntent.putExtra("STUFF", "STUFF");
 startActivity(shareIntent);
+
+//Changed line 5 as per deprecation remarks in API 21+
