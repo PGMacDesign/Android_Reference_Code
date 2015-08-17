78d335700e1cf842e3be3d524ad71bb95033af6b
Patrick MacDowell <PGMacDesign@users.noreply.github.com>
2015-08-17 13:40:55 -0700

    Update Intent.java

--- Java Files/Intent.java
+++ Java Files/Intent.java
@@ -1,7 +1,7 @@
 //This is an example of an intent
 	try{
-		Intent intent1 = new Intent(context, ClassName.class);
-		startActivity(intent1);
+	    Intent intent1 = new Intent(context, ClassName.class);
+	    startActivity(intent1);
 	} catch (Exception e) {
-		e.printStackTrace();
+	    e.printStackTrace();
 	}
