b9b873c068dcde089cf1171403a9171f9cb1eb62
Patrick MacDowell <PGMacDesign@users.noreply.github.com>
2015-09-19 22:55:41 -0700

    Update Intent.java

--- Java Files/Intent.java
+++ Java Files/Intent.java
@@ -1,6 +1,11 @@
-//This is an example of an intent
+	//This is an example of an intent
+	/*
+	Try to run this from within the activity. If running from within a class, make sure to cast! Use:
+	Intent intent1 = new Intent((ParentActivity)getActivity(), NewClassToOpen.class);
+	startActivity(intent1);
+	*/
 	try{
-	    Intent intent1 = new Intent(context, ClassName.class);
+	    Intent intent1 = new Intent(context, ClassToOpen.class);
 	    startActivity(intent1);
 	} catch (Exception e) {
 	    e.printStackTrace();
